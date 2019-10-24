e83521ddb7492df4e416360293fa507ab0b5ae54
Almas Khan <53224806+almaskhanzz@users.noreply.github.com>
2019-10-24 13:12:46 +0600

    Add files via upload

---
+++ controllers/member.js
@@ -0,0 +1,20 @@
+var express = require('express');
+//var userModel = require('../models/user-model');
+var router = express.Router();
+
+router.get('/', function(request, response){
+    var data={
+        username: request.session.username
+    }
+    response.render('member/index', {user:data});
+});
+
+router.get('*', function(request, response, next){
+    if(request.session.username != ""){
+		next();
+	}else{
+		response.redirect('/logout');
+	}
+});
+
+module.exports = router;
--- controllers/registration.js
+++ controllers/registration.js
@@ -6,6 +6,25 @@ router.get('/', function(request, response){
     response.render('registration/index');
 });
 
-
+router.post('/', function(request, response){
+    var user = {
+		name: request.body.name,
+        contactNO: request.body.cno,
+        username: request.body.uname,
+        password: request.body.password,
+        type: request.body.type
+        
+	};
+    console.log(user.type);
+	userModel.insert(user, function(status){
+		
+		if(status){
+            response.redirect('/login');
+			
+		}else{
+			//response.redirect('/admin/addemployee');
+		}
+	});
+});
 
 module.exports = router;
