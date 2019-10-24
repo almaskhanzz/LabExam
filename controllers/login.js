e771e541a9e014dea82c7f05b6cd07c932e6584c
Almas Khan <53224806+almaskhanzz@users.noreply.github.com>
2019-10-24 12:46:39 +0600

    Add files via upload

---
+++ controllers/login.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var usermodel = require('../model/user-model');
+
+var router = express.Router();
+
+router.get('/', function(request, response){
+	res.render('login/index');
+});
+
+router.post('/', function(request, response){
+	if(request.body.submit)
+	{
+		response.redirect('/registration');
+	}else if(request.body.login)
+	{
+		var user = {
+			username: request.body.uname,
+			password: request.body.password
+		};
+		userModel.validate(user, function(result){
+            if(result)
+			{
+				request.session.username=request.body.uname;
+				if(result.type == 1)
+				{
+					response.redirect('/admin');
+				}
+				else if(result.type == 2)
+				{
+					response.redirect('/member');
+				}
+				else
+				{
+					response.redirect('/login');
+				}
+			}
+		});
+		else
+		{
+			response.redirect('/login');
+		}
+	});
+module.exports = router;
